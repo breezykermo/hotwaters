b97a3bd083a7ea983ec7cc849adb606a3ceefecd
Lachlan Kermode <lachiekermode@gmail.com>
2021-05-21 18:47:48 +1200

    Names flying

--- animate.py
+++ animate.py
@@ -1,11 +1,13 @@
 import bpy
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 
-with open("/Users/lachlankermode/Desktop/blender/data/ships.json", "r") as f:
+SCRIPTS_PATH = "/home/lachie/Dropbox (Brown)/blender/blender-scripts/"
+
+with open(SCRIPTS_PATH + "ships.json", "r") as f:
     SHIPS = json.load(f)
 
-with open("/Users/lachlankermode/Desktop/blender/data/ports.json", "r") as f:
+with open(SCRIPTS_PATH + "ports.json", "r") as f:
     PORTS = json.load(f)
 
 port_map = {}
@@ -22,6 +24,15 @@ START_DATE = datetime.strptime('2011-01-01T12:00:00.000Z', '%Y-%m-%dT%H:%M:%S.00
 END_DATE = datetime.strptime('2020-01-01T12:00:00.000Z', '%Y-%m-%dT%H:%M:%S.000Z')
 LENGTH_OF_ANIMATION_IN_FRAMES = 250
 
+DATE_ARRAY = [START_DATE+timedelta(days=x) for x in range((END_DATE-START_DATE).days)]
+FIRST_FRAME = 1
+LAST_FRAME = len(DATE_ARRAY)
+
+def get_closest_date_idx(date):
+    if date is None: return -1
+    value = min(DATE_ARRAY, key=lambda d: abs(d - date))
+    return DATE_ARRAY.index(value)
+
 def create_ship(name):
     location = get_port_template(START_PORT).location
     bpy.data.curves.new(type="FONT",name=name).body = name
@@ -29,27 +40,39 @@ def create_ship(name):
     bpy.context.scene.collection.objects.link(font_obj)
     font_obj.name = name
     font_obj.location = location
-    font_obj.scale = (0.1, 0.1, 0.1)
+    font_obj.scale = (0.2, 0.2, 0.2)
     return font_obj
 
 def get_port_template(port):
     lat = get_lat_from_port(port)
-    dg = str(int((lat + 90.0) * 2)).zfill(3)
+    dg = int((lat + 90.0) * 2) + 160
+    dg = str(dg % 360).zfill(3)
     tmpl = get_object('Ship.' + dg)
     return tmpl
 
-def ship_set_sail(port, name):
-    ship = create_ship(name)
-    tmpl = get_port_template(port)
-    select_object(tmpl)
-    # TODO: set sail...
-
 for ship in SHIPS:
     ship['ports'] = [port_map[x] for x in ship['Port Destination s']]
+
     if ship['Departure'] is not None:
         ship['Departure'] = datetime.strptime(ship['Departure'], '%Y-%m-%dT%H:%M:%S.000Z')
     if ship['Arrival'] is not None:
         ship['Arrival'] = datetime.strptime(ship['Arrival'], '%Y-%m-%dT%H:%M:%S.000Z')
-    print(ship)
-    # ship_set_sail(ship['ports'][0], ship['Name'])
+
+    anim_start = get_closest_date_idx(ship['Departure'])
+    anim_end = get_closest_date_idx(ship['Arrival'])
+
+    # start keyframes
+    new_ship = create_ship(ship['Name'])
+    if anim_start > 0:
+        new_ship.keyframe_insert(data_path="hide_viewport", frame=(anim_start))
+        new_ship.keyframe_insert(data_path='location', frame=(anim_start))
+
+    # end keyframes
+    new_ship.hide_viewport = True
+    new_ship.location = get_port_template(ship['ports'][0]).location
+    new_ship.keyframe_insert(data_path="hide_viewport", frame=anim_end)
+    new_ship.keyframe_insert(data_path="location", frame=anim_end)
+
+    # hide to begin with
+    new_ship.keyframe_insert(data_path="hide_viewport", frame=FIRST_FRAME)
 
