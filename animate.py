46fe0223ed47dead4a0fac792c434fba79c26397
Lachlan Kermode <lachiekermode@gmail.com>
2021-05-27 03:35:44 +1200

    Lights

--- animate.py
+++ animate.py
@@ -6,8 +6,9 @@ from mathutils import Vector
 from math import floor
 from random import randint
 
-SCRIPTS_PATH = "/Users/lachlankermode/Dropbox (Brown)/blender/blender-scripts/"
-FONT_PATH = "/Users/lachlankermode/code/uoa/slowboil.online/assets/union_regular.otf"
+DROPBOX_PREFIX = "/home/lachie"
+SCRIPTS_PATH = DROPBOX_PREFIX + "/Dropbox (Brown)/blender/blender-scripts/"
+FONT_PATH =  DROPBOX_PREFIX + "/Dropbox (Brown)/blender/slowboil/union_regular.otf"
 
 with open(SCRIPTS_PATH + "ships.json", "r") as f:
     SHIPS = json.load(f)
@@ -32,10 +33,6 @@ def warp(port):
     return port
 PORTS = [warp(x) for x in PORTS]
 
-port_map = {}
-for port in PORTS:
-    port_map[port['Name']] = (port['Latitude'], port['Longitude'])
-
 def gs(): return bpy.context.selected_objects[0]
 def get_object(name): return bpy.context.scene.objects.get(name)
 def select_object(o): o.select_set(True)
@@ -45,9 +42,8 @@ def set_hide(obj, vl):
     obj.hide_render = vl
 
 w = 1 # weight
-START_PORT = port_map['El Aaiun']
-START_DATE = datetime.strptime('2011-01-01T12:00:00.000Z', '%Y-%m-%dT%H:%M:%S.000Z')
-END_DATE = datetime.strptime('2020-01-01T12:00:00.000Z', '%Y-%m-%dT%H:%M:%S.000Z')
+START_DATE = datetime.strptime('2011-10-01T12:00:00.000Z', '%Y-%m-%dT%H:%M:%S.000Z')
+END_DATE = datetime.strptime('2021-06-01T12:00:00.000Z', '%Y-%m-%dT%H:%M:%S.000Z')
 LENGTH_OF_ANIMATION_IN_FRAMES = 250
 
 DATE_ARRAY = [START_DATE+timedelta(days=x) for x in range((END_DATE-START_DATE).days)]
@@ -55,6 +51,9 @@ FIRST_FRAME = 1
 TIME_MULTIPLIER = 20
 WHITE_MAT = bpy.data.materials.get("White")
 FRAGMENTS_MAT = bpy.data.materials.get("Fragments")
+PORTS_VECTORS = {}
+SHIP_MATERIALS = {}
+PORT_MATERIALS = {}
 
 def assign_material(mat, ob):
     if ob.data.materials:
@@ -95,6 +94,7 @@ def get_keyframe_no(date):
     return DATE_ARRAY.index(value) * TIME_MULTIPLIER
 
 LAST_FRAME = len(DATE_ARRAY) * TIME_MULTIPLIER
+print("Last frame is: " + str(LAST_FRAME))
 
 def get_date_from_keyframe(frame):
     distance_along = frame / LAST_FRAME
@@ -151,6 +151,11 @@ def create_trail(start_location, end_location, inverse=False):
     ops.object.mode_set(mode='OBJECT')
     return curve
 
+def get_one_yr_in_frames():
+    magic_date = datetime(2012, 10, 1, 12, 0)
+    one_yr_in_frames = DATE_ARRAY.index(magic_date) * TIME_MULTIPLIER
+    return one_yr_in_frames
+
 def animate_ship_with_trail(ship, start_location, end_location, anim_start, anim_end, should_stall=False):
     trail_curve = create_trail(start_location, end_location, inverse=should_stall)
     assign_material(FRAGMENTS_MAT, trail_curve)
@@ -172,55 +177,88 @@ def animate_ship_with_trail(ship, start_location, end_location, anim_start, anim
 
     follow_path.offset_factor = 1.0
     set_hide(ship, True)
-    if not should_stall:
-        ship.keyframe_insert(data_path="hide_viewport", frame=(anim_end))
-        ship.keyframe_insert(data_path="hide_render", frame=(anim_end))
+
+    hide_frame = anim_end if not should_stall else (anim_end + get_one_yr_in_frames())
+
+    ship.keyframe_insert(data_path="hide_viewport", frame=(hide_frame))
+    ship.keyframe_insert(data_path="hide_render", frame=(hide_frame))
+
     follow_path.keyframe_insert(data_path='offset_factor', frame=(anim_end))
 
     # animate bevel factor along path
-    ops.curve.primitive_bezier_circle_add(location=(0.0, 0.0, 0.0), rotation=(0.0, 0.0, 0.0))
-    tube = get_object('ReferenceTube')
-    trail_curve.data.bevel_mode = 'OBJECT'
-    trail_curve.data.bevel_object = tube
-
+    # ops.curve.primitive_bezier_circle_add(location=(0.0, 0.0, 0.0), rotation=(0.0, 0.0, 0.0))
+    # tube = get_object('ReferenceTube')
+    # trail_curve.data.bevel_mode = 'OBJECT'
+    # trail_curve.data.bevel_object = tube
+    #
     set_hide(trail_curve, True)
     trail_curve.keyframe_insert(data_path='hide_viewport', frame=(FIRST_FRAME))
     trail_curve.keyframe_insert(data_path='hide_render', frame=(FIRST_FRAME))
-    trail_curve.data.bevel_factor_end = 0.0
+    # trail_curve.data.bevel_factor_end = 0.0
     set_hide(trail_curve, False)
-    b1_kf = trail_curve.data.keyframe_insert(data_path='bevel_factor_end', frame=(anim_start))
+    # b1_kf = trail_curve.data.keyframe_insert(data_path='bevel_factor_end', frame=(anim_start))
     trail_curve.keyframe_insert(data_path='hide_viewport', frame=(anim_start))
     trail_curve.keyframe_insert(data_path='hide_render', frame=(anim_start))
-    trail_curve.data.bevel_factor_end= 1.0
+    # trail_curve.data.bevel_factor_end= 1.0
     set_hide(trail_curve, True)
-    b2_kf = trail_curve.data.keyframe_insert(data_path='bevel_factor_end', frame=(anim_end))
-    if not should_stall:
-        trail_curve.keyframe_insert(data_path='hide_viewport', frame=(anim_end))
-        trail_curve.keyframe_insert(data_path='hide_render', frame=(anim_end))
+    # b2_kf = trail_curve.data.keyframe_insert(data_path='bevel_factor_end', frame=(anim_end))
+    trail_curve.keyframe_insert(data_path='hide_viewport', frame=(hide_frame))
+    trail_curve.keyframe_insert(data_path='hide_render', frame=(hide_frame))
 
     # TODO: work out how to do this
     # for fc in trail_curve.animation_data.action.fcurves:
     #     fc.extrapolation = 'LINEAR' # Set extrapolation type
 
+def get_color(mat):
+    return mat.node_tree.nodes["Emission"].inputs[0].default_value
+
+def keyframe_color(mat, vl, frame):
+    mat.node_tree.nodes["Emission"].inputs[0].default_value = vl
+    ei = mat.node_tree.nodes["Emission"].inputs[0]
+    ei.keyframe_insert(data_path='default_value', frame=frame)
+
+
+glows = [0, 48, 96, 144]
+white_color = (1.0, 1.0, 1.0, 1.0)
+light_color = (1.0, 1.0, 0.5, 1.0)
+
+def light_up_ports(ports, start_frame):
+    print("Lighting up " + str(ports) + " at frame " + str(start_frame))
+
+    anim_starts = [t+start_frame-12 for t in glows]
+    anim_mids = [t+start_frame+24 for t in glows]
+    anim_ends = [t+start_frame+72 for t in glows]
+
+    for idx, port in enumerate(ports):
+        original_color = get_color(PORT_MATERIALS[port])
+        keyframe_color(PORT_MATERIALS[port], white_color, anim_starts[idx])
+        keyframe_color(PORT_MATERIALS[port], light_color, anim_mids[idx])
+        keyframe_color(PORT_MATERIALS[port], white_color, anim_ends[idx])
+
+
 def set_sail(ship):
     anim_start = floor(get_keyframe_no(ship['Departure']))
     anim_end = floor(get_keyframe_no(ship['Arrival']))
-    start_location = get_port_template(START_PORT).location
-    end_location = Vector((0,0,0)) if anim_end < 0 else get_port_template(ship['ports'][0]).location
+    start_location = PORTS_VECTORS['El Aaiun']
+    ship_first_port = ship['Port Destination s'][0]
+    end_location = PORTS_VECTORS[ship_first_port]
+
 
     new_ship = create_text(ship['Name'], start_location, scale=(1,1,1), hidden=True)
-    assign_material(WHITE_MAT, new_ship)
+
+    SHIP_MATERIALS[ship['Name']] = WHITE_MAT.copy()
+    assign_material(SHIP_MATERIALS[ship['Name']], new_ship)
     should_stall = False
 
     if ship['Name'] == 'NM Cherry Blossom':
-        end_location = Vector((end_location.x, end_location.y + 2, end_location.z))
+        end_location = Vector((end_location.x, end_location.y + 1, end_location.z))
         should_stall = True
 
-    # animate_ship_attempt_1(new_ship, start_location, end_location, anim_start, anim_end)
-    # animate_curve_attempt_1(start_location, end_location, anim_start, anim_end)
-
     context.scene.frame_set(FIRST_FRAME)
+
     animate_ship_with_trail(new_ship, start_location, end_location, anim_start, anim_end, should_stall=should_stall)
+    light_up_ports(ship["Port Destination s"], anim_end)
+
 
 def add_port_name(port):
     loc = ship_tmpl_from_lat(port['Latitude']).location.copy()
@@ -232,15 +270,16 @@ def add_port_name(port):
     if (port["Name"] == "Port Elizabeth"):
         loc.y = loc.y + 2.5
 
-    create_text(port["display_name"], loc, scale=(0.8,0.8,0.8))
+    new_port = create_text(port["display_name"], loc, scale=(0.8,0.8,0.8))
+    PORT_MATERIALS[port['Name']] = WHITE_MAT.copy()
+    assign_material(PORT_MATERIALS[port['Name']], new_port)
+    PORTS_VECTORS[port["Name"]] = loc
 
 def animate_ships():
     for port in PORTS:
         add_port_name(port)
 
     for ship in SHIPS:
-        ship['ports'] = [port_map[x] for x in ship['Port Destination s']]
-
         if ship['Departure'] is not None:
             ship['Departure'] = datetime.strptime(ship['Departure'], '%Y-%m-%dT%H:%M:%S.000Z')
         if ship['Arrival'] is not None:
